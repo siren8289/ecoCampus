b43c262e299a9c40c339bf0fda18d795d2516ffa
siren8289 <siren8289@gmail.com>
2026-01-31 18:35:24 +0900

    백엔드 수정 1차

--- spring/api/src/main/java/com/ecocampus/api/entrylog/EntryLogController.java
+++ spring/api/src/main/java/com/ecocampus/api/entrylog/EntryLogController.java
@@ -1,5 +1,7 @@
 package com.ecocampus.api.entrylog;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/entrylogs")
 @RequiredArgsConstructor
+@Tag(name = "EntryLog", description = "출입 기록 관리 API")
 public class EntryLogController {
     private final EntryLogService entryLogService;
+
+    @org.springframework.web.bind.annotation.GetMapping
+    @Operation(summary = "테스트 엔드포인트", description = "Swagger UI 작동 확인을 위한 임시 API입니다.")
+    public String hello() {
+        return "Hello Swagger!";
+    }
 }
